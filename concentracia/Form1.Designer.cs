7f1900c33d0d0622db06ee436be96a4e66c24243
poguk12 <okrodik@gmail.com>
2026-01-29 13:03:10 +0900

    Добавьте файлы проекта.

---
+++ concentracia/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace concentracia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+    }
+}
+
---
+++ concentracia/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace concentracia
+{
+    public partial class Form1 : Form
+    {
+        Panel panel1 = new Panel();
+        PictureBox[,] pictureBoxes = new PictureBox[10, 10];
+        int width = 15;
+        int height = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            settingWindow();
+            createPole();
+        }
+
+        private void settingWindow()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            menuStrip1.BackColor = Color.LightCyan;
+            panel1.Dock = DockStyle.Fill;
+            panel1.BackColor = Color.LightCoral;
+
+            this.Controls.Add(panel1);
+        }
+
+        private void createPole()
+        {
+            panel1.Controls.Clear();
+
+            pictureBoxes = new PictureBox[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    int sizeX = 150;
+                    int sizeY = 80;
+
+                    pictureBoxes[i, j] = new PictureBox();
+                    pictureBoxes[i, j].Location = new Point(30 + (sizeX + 10) * i, 50 + (sizeY + 10) * j);
+                    pictureBoxes[i, j].Size = new Size(sizeX, sizeY);
+                    pictureBoxes[i, j].Name = i.ToString() + j.ToString();
+                    pictureBoxes[i,j].BackColor = Color.LightCyan;
+                    pictureBoxes[i, j].BorderStyle = BorderStyle.Fixed3D;
+
+                    panel1.Controls.Add(pictureBoxes[i,j]);
+                }
+            }
+        }
+    }
+}
