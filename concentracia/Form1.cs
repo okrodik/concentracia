ea66ade532006dd572262d9ba0280c34d759dd2e
poguk12 <okrodik@gmail.com>
2026-02-02 13:05:44 +0900

    доавбить сравнение

--- concentracia/Form1.cs
+++ concentracia/Form1.cs
@@ -15,10 +15,12 @@ namespace concentracia
     {
         Panel panel1 = new Panel();
         PictureBox[,] pictureBoxes = new PictureBox[10, 10];
-        int width = 12;
+        int width = 6;
         int height = 6;
+        int rachet = 0;
 
         int[] photoMassiv = new int[2];
+        PictureBox[] para = new PictureBox[2];
 
         Random random = new Random();
 
@@ -28,7 +30,7 @@ namespace concentracia
             settingWindow();
             createPole();
             addPhoto();
-            scrit();
+            //scrit();
         }
 
         private void settingWindow()
@@ -45,32 +47,38 @@ namespace concentracia
         private void createPole()
         {
             panel1.Controls.Clear();
+            int cshet = 0;
 
             pictureBoxes = new PictureBox[width, height];
 
-            for (int i = 0; i < width; i++)
+            for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < height; j++)
+                for (int j = 0; j < width; j++)
                 {
                     int sizeX = 60;
                     int sizeY = 60;
 
+
                     pictureBoxes[i, j] = new PictureBox();
-                    pictureBoxes[i, j].Location = new Point(30 + (sizeX + 10) * i, 50 + (sizeY + 10) * j);
+                    pictureBoxes[i, j].Location = new Point(30 + (sizeX + 10) * j, 50 + (sizeY + 10) * i);
                     pictureBoxes[i, j].Size = new Size(sizeX, sizeY);
                     pictureBoxes[i, j].Name = i.ToString() + j.ToString();
                     pictureBoxes[i,j].BackColor = Color.LightCyan;
                     pictureBoxes[i, j].BorderStyle = BorderStyle.Fixed3D;
                     pictureBoxes[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    pictureBoxes[i, j].Click += new EventHandler(clickPicture);
+                    pictureBoxes[i, j].Tag = cshet;
 
                     panel1.Controls.Add(pictureBoxes[i,j]);
+
+
+                    cshet += 1;
                 }
             }
         }
 
         private void addPhoto()
         {
-            int x= 0;
             int xx = 0;
             int yyy = 1;
             int delenie = (width * height) / 2;
@@ -117,10 +125,45 @@ namespace concentracia
             {
                 for (int j = 0; j < height; j++)
                 {
-                    pictureBoxes[i, j].BackColor = Color.Red;
+                    pictureBoxes[i, j].Image = Image.FromFile("C:\\Users\\student1\\Downloads\\bc.png");
 
                 }
             }
         }
+
+        private void clickPicture(object sender, EventArgs e)
+        {
+            var pictureBox = sender as PictureBox;
+
+           openPhoto( pictureBox );
+        }
+
+        private void openPhoto(object sender)
+        {
+            
+            var pictureBox = sender as PictureBox;
+            pictureBox.Image = Image.FromFile($"C:\\Users\\student1\\Desktop\\Новая папка\\{photoMassiv[Convert.ToInt32(pictureBox.Tag)]}.jpg");
+
+            para[rachet++] = (pictureBox);
+
+            if (rachet >= 2)
+            {
+                int index1 = Convert.ToInt32(para[0].Tag);
+                int index2 = Convert.ToInt32(para[1].Tag);
+
+                photoMassiv 
+
+                if (index1 == index2)
+                    MessageBox.Show("Изображения совпадают!");
+                else
+                    MessageBox.Show("различны.");
+                    //para[0].Image = Image.FromFile($"C:\\Users\\student1\\Desktop\\Новая папка\\{photoMassiv[Convert.ToInt32(para[0].Tag)]}.jpg");
+                    //para[1].Image = Image.FromFile($"C:\\Users\\student1\\Desktop\\Новая папка\\{photoMassiv[Convert.ToInt32(para[1].Tag)]}.jpg");
+
+                Array.Clear(para, 0, para.Length);
+                rachet = 0;
+            }
+        }
+
     }
 }
